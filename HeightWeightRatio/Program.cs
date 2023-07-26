eec02208c4e196c7ea77575c87f948910545b535
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-07-26 18:30:52 +0300

    Proje dosyası ekle.

---
+++ HeightWeightRatio/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeightWeightRatio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // *** Height and Weight ratio ***
+
+            Heading();
+            double height = Start("How tall are you ? ");
+            double weight = Start("What's your weight ? ");
+            double ratio = RatioCalculation(height,weight);
+            Console.WriteLine("Your height and weight ratio is : " + ratio);
+            RatioResult(ratio);
+
+
+            Console.Read();
+        }
+
+       
+        static double RatioCalculation(double height,double weight)
+        {
+            double ratio = weight / (height * height);
+            return ratio;
+        }
+        static void RatioResult(double ratio)
+        {
+            Console.Write("Your height and weight ratio is : ");
+            if (ratio < 18)
+                Console.WriteLine("Weak");
+            if (ratio >= 18 && ratio < 25)
+                Console.WriteLine("Normal");
+            if (ratio >= 25 && ratio < 30)
+                Console.WriteLine("Fat");
+            if (ratio >= 30 && ratio < 35)
+                Console.WriteLine("Obese");
+            
+        }
+        static void Heading()
+        {
+            Console.WriteLine("*** Height and Weight Ratio ***");
+            Console.WriteLine();
+            Console.WriteLine("You should use < , > if you need it");
+            Console.WriteLine();
+        }
+        
+        static double Start(string message)
+        {
+            double start;
+            Console.WriteLine(message);
+            start = Convert.ToDouble(Console.ReadLine());
+            return start;
+        }
+    }
+}
---
+++ HeightWeightRatio/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("HeightWeightRatio")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HeightWeightRatio")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("bd9f1bc7-88d4-4093-8f4d-1d4c3a4b0e3f")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
